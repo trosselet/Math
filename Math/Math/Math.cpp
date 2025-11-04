8f1d614061a510b2591b38ca439766b89611c649
Thomas <trosselet@gaming.tech>
2025-11-04 16:03:42 +0100

    Exo 1.2

--- Math/Math/Math.cpp
+++ Math/Math/Math.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <Windows.h>
+
+
+void ClearConsole()
+{
+    std::cout << "\x1B[2J";
+}
+
+void ShowCursor()
+{
+    std::cout << "\x1B[?25h";
+}
+
+void HideCursor()
+{
+    std::cout << "\x1B[?25l";
+}
+
+void SetCursorStartPos()
+{
+    std::cout << "\x1B[H";
+}
 
 int main(int argc, char** argv)
 {
     std::cout << "Hello World!\n";
+
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD mode;
+    GetConsoleMode(hConsole, &mode);
+    SetConsoleMode(hConsole, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+
+    ClearConsole();
+
+    HideCursor();
+    ShowCursor();
+    SetCursorStartPos();
 
     return 0;
 }
