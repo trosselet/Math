833873e5ffde92c40026daf92139c83321c7b569
Thomas <trosselet@gaming.tech>
2025-11-04 16:04:22 +0100

    Exo 1.3

--- Math/Math/Math.cpp
+++ Math/Math/Math.cpp
@@ -22,20 +22,47 @@ void SetCursorStartPos()
     std::cout << "\x1B[H";
 }
 
+void SetConsoleSize(int width, int height)
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    COORD bufferSize;
+    bufferSize.X = width;
+    bufferSize.Y = height;
+    SetConsoleScreenBufferSize(hConsole, bufferSize);
+
+    SMALL_RECT windowSize;
+    windowSize.Left = 0;
+    windowSize.Top = 0;
+    windowSize.Right = width - 1;
+    windowSize.Bottom = height - 1;
+    SetConsoleWindowInfo(hConsole, TRUE, &windowSize);
+}
+
 int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+    ClearConsole();
+    SetCursorStartPos();
 
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
     DWORD mode;
     GetConsoleMode(hConsole, &mode);
     SetConsoleMode(hConsole, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
 
-    ClearConsole();
+    SetConsoleSize(100, 20);
 
     HideCursor();
+    for (int i = 0; i < 20; i++)
+    {
+        for (int j = 0; j < 100; j++)
+        {
+            std::cout << '.';
+        }
+    }
+
+
     ShowCursor();
-    SetCursorStartPos();
 
     return 0;
 }
